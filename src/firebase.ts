418e2d75b64139e8ba8a2753190f6b486f5d23ed
07-jin-FUK <167975182+07-jin-FUK@users.noreply.github.com>
2024-11-19 22:40:22 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDL0Mk4y-PCLNv1lSi8pNH8NPYts41mwTo",
-    authDomain: "save-1fcc0.firebaseapp.com",
-    projectId: "save-1fcc0",
-    storageBucket: "save-1fcc0.appspot.com",
-    messagingSenderId: "940558153116",
-    appId: "1:940558153116:web:928b2ecf70d9c6540e8459"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
